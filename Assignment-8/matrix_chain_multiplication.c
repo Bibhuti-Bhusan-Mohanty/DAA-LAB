9e82882019652952b3fb626301feae7cd7b6fb4c
BIBHUTI BHUSAN MOHANTY <bibhutibhusanmohanty6504@gmail.com>
2024-05-25 23:46:51 +0530

    All Checked!

--- Assignment-8/matrix_chain_multiplication.c
+++ Assignment-8/matrix_chain_multiplication.c
@@ -7,9 +7,9 @@ int s[100][100];
 void matrixChainOrder(int p[], int n) {
   for (int i = 1; i < n; i++)
     m[i][i] = 0;
-  for (int L=2; L<n; L++) {
-    for (int i=1; i<n-L+1; i++) {
-      int j = i+L-1;
+  for (int l=2; l<n; l++) {
+    for (int i=1; i<n-l+1; i++) {
+      int j = i+l-1;
       m[i][j] = 99999;
       for (int k=i; k<=j-1; k++) {
         int q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
